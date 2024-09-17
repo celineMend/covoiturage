deda796eb6fba47cd13bb39d8c74f8044cb0d012
celinamendy <celine.mendy@unchk.edu.sn>
2024-09-17 16:20:01 +0100

    Finalisation de la fonctionnalité utilisateur

--- covoiturage/database/seeders/RoleSeeder.php
+++ covoiturage/database/seeders/RoleSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Création des rôles
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'conducteur']);
+        Role::create(['name' => 'passager']);
     }
 }
